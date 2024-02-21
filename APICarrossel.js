a062f6e47939e6cfeb11396a16a4db53e455c6ab
valdemineto <147013277+valdemineto@users.noreply.github.com>
2024-02-21 20:47:31 -0300

    APICarrossel.js

--- APICarrossel.js
+++ APICarrossel.js
@@ -1,22 +1,31 @@
 let cont = 1;
+let click = 0;
 document.getElementById("radio1").checked = true;
 let interval = 3000;
-let inter = setInterval(navigatorAuto, interval);
 function resetCont(reset) {
     cont = reset;
 }
+function rein() {
+    if (click == 0) {
+        setInterval(navigatorAuto, interval);
+    }
+};
+let inter = setInterval(navigatorAuto, interval);
 function navigatorAuto() {
     cont++
     if (cont > 4) {
         cont = 1;
     }
     document.getElementById("radio" + cont).checked = true;
+    click = 1;
 }
+
 function anterior() {
     if (cont != 1) {
         clearInterval(inter)
         cont--
         document.getElementById('radio' + cont).checked = true;
+        click = 1;
     }
 }
 function proximo() {
